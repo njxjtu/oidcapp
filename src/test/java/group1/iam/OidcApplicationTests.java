8dd8aac00fe53c9b9608a55e8289c959460f2e11
Nan Jiang <njxjtu@gmail.com>
2025-11-24 23:46:06 -0800

    Initial checkin

---
+++ src/test/java/group1/iam/OidcApplicationTests.java
@@ -0,0 +1,13 @@
+package group1.iam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OidcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
